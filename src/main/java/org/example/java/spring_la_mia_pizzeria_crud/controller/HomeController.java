0e53e9ec315964d6801f4f85adf076d51744222a
AndreaCatapano <a.catapano96@gmail.com>
2025-07-14 18:28:46 +0200

    feat: added bootstrap by cdn

---
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.java.spring_la_mia_pizzeria_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model) {
+        return "home";
+    }
+
+}
--- src/main/java/org/example/java/spring_la_mia_pizzeria_crud/repo/PizzaRepository.java
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/repo/PizzaRepository.java
@@ -1,5 +1,6 @@
 package org.example.java.spring_la_mia_pizzeria_crud.repo;
 
+import org.example.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
