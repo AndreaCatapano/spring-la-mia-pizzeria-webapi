22a8523706c48ef8bed91893d8083c7e69dd7a40
AndreaCatapano <a.catapano96@gmail.com>
2025-07-14 18:03:36 +0200

    first commit

---
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,81 @@
+package org.example.java.spring_la_mia_pizzeria_crud.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza implements Serializable {
+
+    public Pizza() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(name = "descrizione", nullable = false)
+    private String description;
+
+    @NotBlank
+    @Column(name = "foto", nullable = false)
+    private String url;
+
+    @NotNull
+    @Column(name = "prezzo", nullable = false, precision = 5, scale = 2)
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/repo/PizzaRepository.java
@@ -0,0 +1,7 @@
+package org.example.java.spring_la_mia_pizzeria_crud.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+}
