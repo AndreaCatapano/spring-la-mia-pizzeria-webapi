0f920eb1a6bfbfff83dca68fb42ae8cb15d1ecd7
AndreaCatapano <a.catapano96@gmail.com>
2025-07-21 15:38:33 +0200

    feat: created promotion class

---
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/model/Promotion.java
@@ -0,0 +1,70 @@
+package org.example.java.spring_la_mia_pizzeria_crud.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "promotions")
+public class Promotion implements Serializable {
+
+    public Promotion() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @NotBlank
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+}
