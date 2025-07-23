795e6dac97af8943ef2e02cdf7f972706ddb7c9d
AndreaCatapano <a.catapano96@gmail.com>
2025-07-23 16:02:23 +0200

    feat: added delete path

--- src/main/java/org/example/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -77,4 +77,11 @@ public class IngredientController {
         return "redirect:/ingredients";
     }
 
+    @PostMapping("delete/{id}")
+    public String delete(@PathVariable int id) {
+
+        repository.deleteById(id);
+        return "redirect:/ingredients";
+    }
+
 }
