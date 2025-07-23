56668323f8cc00acf5eefdb71555640a1b890b1c
AndreaCatapano <a.catapano96@gmail.com>
2025-07-23 15:41:17 +0200

    feat: added show and create in Ingredient

--- src/main/java/org/example/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/example/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -7,11 +7,19 @@ import org.example.java.spring_la_mia_pizzeria_crud.repo.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/ingredient")
+@RequestMapping("/ingredients")
 public class IngredientController {
 
     @Autowired
@@ -21,7 +29,39 @@ public class IngredientController {
     public String index(Model model) {
         List<Ingredient> ingredients = repository.findAll();
         model.addAttribute("ingredients", ingredients);
-        return "ingredient/index";
+        return "ingredients/index";
+    }
+
+    @GetMapping("show/{id}")
+    public String getMethodName(@PathVariable int id, Model model) {
+        Ingredient ingredient = repository.findById(id).orElseThrow();
+
+        if (ingredient != null) {
+            model.addAttribute("ingredient", ingredient);
+
+            return "ingredients/show";
+        }
+
+        return "ingredients/index";
+    }
+
+    @GetMapping("/create")
+    public String getCreate(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
+        return "ingredients/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "ingredients/create";
+        }
+
+        repository.save(formIngredient);
+
+        return "redirect:/ingredients";
     }
 
 }
